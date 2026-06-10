8c0a1f00f73f2b5429b7c88a8383f593e4d53d2b
harshitnagar09 <harshitnagar043@gmail.com>
2026-06-10 23:41:57 +0530

    Add new file account_types.py to define different types of bank accounts and their specific behaviors.

---
+++ account_types.py
@@ -0,0 +1,11 @@
+from main import BankAccount
+
+class SavingsAccount(BankAccount):
+    def __init__(self, balance, account_no, name, pin, interest_rate):
+        super().__init__(???)        _
+        self.interest_rate = ???     
+
+    def apply_interest(self):
+        interest = ???              
+        self.credit(???)             
+        print(???)   
